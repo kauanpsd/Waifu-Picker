2aeba91b3938dc27ea089dc48e0a5ff371c32e77
Kauan Carvalho <kauan3210@gmail.com>
2023-06-04 10:24:59 -0300

    Radio Box fixed

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import { waifusData } from './data.js'
 
 const emotionRadioDiv = document.getElementById("emotion-radios")
+const getBtn = document.getElementById('get-image')
+
+function getMatchWaifu(){
+
+    const waifuvalue = document.querySelector( 'input[type="radio"]:checked')
+    if(waifuvalue){
+        console.log(waifuvalue.value)
+    }
+    
+}
+
+
+getBtn.addEventListener('click',getMatchWaifu)
 
 emotionRadioDiv.addEventListener('change', function(e){
-    console.log(e.target.id)
+    const radios = document.getElementsByClassName('radio');
+    for(let radio of radios){
+        radio.classList.remove('highlight')
+    }
+       document.getElementById(e.target.id).parentElement.classList.add('highlight')
 })
 
 function getEmotionsArray(waifus){
@@ -19,7 +36,7 @@ function getEmotionsArray(waifus){
     return emotionList
 }
 
-function renderEmotionsRadios(waifus){
+function renderEmotionsRadios(waifus){ 
     const emotions = getEmotionsArray(waifus)
     let radioItems = ``
     for (let emotion of emotions){
